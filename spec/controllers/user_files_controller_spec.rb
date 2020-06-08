03d0f634d507bd514a25d1e500a5b1c22bf93af9
daiki510 <44395706+daiki510@users.noreply.github.com>
2020-06-08 21:54:57 +0900

    specの追加

--- spec/controllers/user_files_controller_spec.rb
+++ spec/controllers/user_files_controller_spec.rb
@@ -3,9 +3,19 @@ require 'rails_helper'
 RSpec.describe UserFilesController, type: :controller do
 
   describe "GET #index" do
-    it "returns http success" do
+    before do
+      @users = create_list(:user, 10)
+    end
+
+    it "レスポンスに成功すること" do
       get :index
       expect(response).to have_http_status(:success)
+    end
+
+    it "zip形式のデータが返ってくること" do
+      get :index
+      binding.pry
+      expect(response["Content-Type"]).to eq "application/zip"
     end
   end
 
