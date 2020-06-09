be6e85be6e9fe3c7075d6c7b1f67c886765e3acd
daiki510 <44395706+daiki510@users.noreply.github.com>
2020-06-10 06:06:48 +0900

    zip生成のspec追加

---
+++ spec/models/user_zip_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe UserZip, type: :model do
+  describe "#generate" do
+    before do
+      @users = create_list(:user, 10)
+    end
+
+    it "zipファイルが生成されること" do
+      zip_file = UserZip.new(@users).generate
+      expect(File.extname(zip_file.path)).to eq ".zip"
+
+      # zipファイルの中身を確認する
+      Zip::File.open(zip_file.path) do |zipfile|
+        zipfile.each_with_index do |entry, i|
+          expect(entry.name).to eq "user_#{@users[i].id}.csv"
+        end
+      end
+    end
+  end
+end
